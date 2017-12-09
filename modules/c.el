bd4427d806443ccb92ba27b7aa24713e4d10079e
lillypad <lchalupowski@gosecure.ca>
2017-12-08 22:34:19 -0400

    C Mode Hook Bug Fix

--- modules/c.el
+++ modules/c.el
@@ -8,11 +8,15 @@
 (add-to-list 'company-backends 'company-c-headers)
 
 (defun my/c-mode-common-hook ()
+  "C Programming Mode Hook."
+  (autopair-mode)
   (setq c-basic-offset 2)
   (setq auto-mode-alist
         (append
          '(("CMakeLists\\.txt\\'" . cmake-mode))
          '(("\\.cmake\\'" . cmake-mode))
          auto-mode-alist)))
+
+(add-hook 'c-mode-hook 'my/c-mode-common-hook)
 
 ;;; c.el ends here
