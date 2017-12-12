93b0365da44c7caa6103c4529b5479c0c466c551
lillypad <lchalupowski@gosecure.ca>
2017-12-12 08:02:28 -0400

    lisp/slime

--- modules/lisp.el
+++ modules/lisp.el
@@ -5,6 +5,8 @@
 
 ;;; Code:
 
+
+
 (defun my/lisp-mode-hook ()
   "Lisp Mode Hook with Slime."
   (if (= (shell-command "which sbcl") 0)
@@ -12,7 +14,9 @@
     (if (= (shell-command "which clisp") 0)
         (setq inferior-lisp-program (shell-command-to-string "which clisp"))
       ()))
+  (add-to-list 'company-backends 'slime-company)
   (setq slime-contribs '(slime-fancy))
+  (slime-setup '(slime-fancy slime-company))
   (autopair-mode))
 
 (add-hook 'lisp-mode-hook 'my/lisp-mode-hook)
