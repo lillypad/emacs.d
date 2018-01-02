0aa3fa4c8e427693c9b30cf01c3fe78d390f7edd
lillypad <lchalupowski@gosecure.ca>
2018-01-02 11:42:33 -0400

    bug fix powerline glifs

--- modules/theme.el
+++ modules/theme.el
@@ -7,6 +7,15 @@
 
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 (load-theme 'airline-doom-molokai t)
+(setq powerline-utf-8-separator-left        #xe0b0
+      powerline-utf-8-separator-right       #xe0b2
+      airline-utf-glyph-separator-left      #xe0b0
+      airline-utf-glyph-separator-right     #xe0b2
+      airline-utf-glyph-subseparator-left   #xe0b1
+      airline-utf-glyph-subseparator-right  #xe0b3
+      airline-utf-glyph-branch              #xe0a0
+      airline-utf-glyph-readonly            #xe0a2
+      airline-utf-glyph-linenumber          #xe0a1)
 (load-theme 'badwolf t)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
